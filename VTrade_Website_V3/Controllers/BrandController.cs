4976ab55d9a7965870d678eb6a4a1f382cc2080e
sennthilkumaark <sennthilkumaar.k@gmail.com>
2023-03-19 23:15:30 +0530

    brand module updated for pagination

--- VTrade_Website_V3/Controllers/BrandController.cs
+++ VTrade_Website_V3/Controllers/BrandController.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using System.Web.Mvc;
-using VTrade_Website_V3.Models;
 using DAL.Repository;
 using DAL.Models;
 using DAL;
+using VTrade_Website_V3.Models;
 
 namespace VTrade_Website_V3.Controllers
 {
@@ -19,75 +16,49 @@ namespace VTrade_Website_V3.Controllers
         }
 
         [HttpGet]
-        public JsonResult GetBrandItems(int PageNO)
+        public PartialViewResult GetBrandItems(int PageNO)
         {
-            AdditionalListResponseData res = new AdditionalListResponseData();
-            try
+            BrandListResponseData res = new BrandListResponseData();
+            Methods Repobj = new Methods();
+            List<BrandItem> lstObj = new List<BrandItem>();
+
+            _getBrandListItems _getBrandItemsObj = new _getBrandListItems();
+            _getBrandItemsObj = Repobj.getBrandListItems(PageNO, 6);
+
+            if (_getBrandItemsObj.ResponseStatus == true)
             {
-
-                Methods Repobj = new Methods();
-                _getBrandListItems _getBrandItemsObj = new _getBrandListItems();
-                _getBrandItemsObj = Repobj.getBrandListItems(PageNO, 6);
-
-                if (_getBrandItemsObj.ResponseStatus == true)
+                if (_getBrandItemsObj.lstBrandItem != null)
                 {
-                    List<BrandItem> lstObj = new List<BrandItem>();
                     lstObj = _getBrandItemsObj.lstBrandItem;
-                    string str_responseData = "";
+                }
+            }
 
-                    if (lstObj != null)
-                    {
-                        foreach (BrandItem varBrandListItem in lstObj)
-                        {
-                            str_responseData += "<div class='col-lg-4 col-md-6 Brand-item'> <a href='/product?BrandID=" + varBrandListItem.ID + "'><img src='" + varBrandListItem.BrandImgPath + "' class='img-Brand' alt=''/></a>";
-                            str_responseData += "<div class='Brand-info'><a href='/product?BrandID=" + varBrandListItem.ID + "'>";
-                            str_responseData += "<h4>" + varBrandListItem.BrandName + "</h4>";
-                            str_responseData += "</a></div></div>";
-                        }
-                    }
+            res.lstBrandItem = lstObj;
+            res.PageNO = _getBrandItemsObj.PageNO;
+            res.NumSize = _getBrandItemsObj.numSize;
 
-                    res.ResponseSuccess = true;
-                    res.ResponseMessage = str_responseData;
-                    res.PageNO = _getBrandItemsObj.PageNO;
-                    res.NumSize = _getBrandItemsObj.numSize;
-                    int StartPg = _getBrandItemsObj.StartPg;
-                    int TotalPg = _getBrandItemsObj.TotalPg;
+            if (lstObj.Count > 0)
+            {
+                int StartPg = _getBrandItemsObj.StartPg;
+                int TotalPg = _getBrandItemsObj.TotalPg;
+                int StartCount = (StartPg + 1);
+                int EndCount = (StartPg + lstObj.Count);
 
-                    if (lstObj.Count() > 0)
-                    {
-                        int StartCount = (StartPg + 1);
-                        int EndCount = (StartPg + lstObj.Count());
-
-                        if (EndCount > StartCount)
-                        {
-                            res.ResponseMessage2 = "Showing results of " + StartCount + " - " + EndCount + " out of " + TotalPg + " brands";
-                        }
-                        else
-                        {
-                            res.ResponseMessage2 = "Showing results of " + StartCount + " out of " + TotalPg + " brands";
-                        }
-                    }
-                    else
-                    {
-                        res.ResponseMessage2 = "Showing results 0 brands";
-                    }
+                if (EndCount > StartCount)
+                {
+                    res.PageDesc = "Showing results of " + StartCount + " - " + EndCount + " out of " + TotalPg + " brands";
                 }
                 else
                 {
-                    res.ResponseSuccess = false;
-                    //res.ResponseMessage = _getBrandItemsObj.ErrorMessage;
-                    res.ResponseMessage = "The server has encountered an unexpected internal error. Please try again later.";
+                    res.PageDesc = "Showing results of " + StartCount + " out of " + TotalPg + " brands";
                 }
             }
-            catch (Exception ex)
+            else
             {
-                res.ResponseSuccess = false;
-                res.ResponseMessage = "The server has encountered an unexpected internal error. Please try again later.";
-                //res.ResponseMessage = ex.Message.ToString();
+                res.PageDesc = "Showing results 0 brands";
             }
 
-            return Json(res, JsonRequestBehavior.AllowGet);
-
+            return PartialView("GetBrandItems", res);
         }
     }
 }
---
+++ VTrade_Website_V3/Models/BrandListResponseData.cs
@@ -0,0 +1,16 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VTrade_Website_V3.Models
+{
+    public class BrandListResponseData
+    {
+        public List<BrandItem> lstBrandItem { get; set; }
+        public string PageDesc { get; set; }
+        public int PageNO { get; set; }
+        public int NumSize { get; set; }
+    }
+}
